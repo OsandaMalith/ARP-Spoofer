d952e4a8a17c51b95fc1311340ef739d4aca2092
Osanda Malith Jayathissa <osandajayathissa@gmail.com>
2015-07-01 23:42:15 -0700

    Update arp.py

--- arp.py
+++ arp.py
@@ -6,6 +6,21 @@ import threading
 
 # Author: Osanda Malith Jayathissa
 # http://osandamalith.wordpress.com
+'''
+        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+                    Version 2, December 2004 
+
+ Copyright (C) 2015 Osanda Malith Jayathissa <osanda@unseen.is> 
+
+ Everyone is permitted to copy and distribute verbatim or modified 
+ copies of this license document, and changing it is allowed as long 
+ as the name is changed. 
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+'''
 
 class ARPObj(object):
     """The main class for the ARP Spoofer"""
